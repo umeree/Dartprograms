2b3fc0f219a9efc70f8c3cb171ae210dc70aefe0
Umer <umer@Usmans-MacBook-Pro.local>
2023-11-13 18:07:09 +0500

    11 November

---
+++ ConstructorInheritance.dart
@@ -0,0 +1,20 @@
+class Person{
+   String name;
+  int age;
+
+
+  Person(this.name, this.age);
+}
+class Student extends Person{
+  int rollNumber;
+  Student(String name, int age, this.rollNumber ):super(name, age);
+
+}
+
+void main() {
+  var student = Student("John", 20, 1);
+  print("Student name: ${student.name}");
+  print("Student age: ${student.age}");
+  print("Student roll number: ${student.rollNumber}");
+
+}
